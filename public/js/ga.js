81c82c45cd0b189499fbec844c7f6888ab59ed6d
TuanMUVN <kunproyeuem@gmail.com>
2017-03-20 23:04:44 +0700

    Add files via upload

--- public/js/ga.js
+++ public/js/ga.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-90215707-1', 'auto');
+  ga('create', 'UA-83954984-1', 'auto');
   ga('send', 'pageview');
